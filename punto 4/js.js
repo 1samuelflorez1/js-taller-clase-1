43f8803cdd6b83928bfd0529059045af917aa7d3
samuel florez <samuel.f.v.t.p@gmail.com>
2025-03-21 17:19:58 -0500

    se agrego el punto 4

---
+++ punto 4/js.js
@@ -0,0 +1,19 @@
+let marca = prompt("ingrese la marca de la moto");
+let precio = prompt("ingrese el precio de la moto");
+let descuento = 0
+
+if (marca === "Honda" || marca == "honda" || marca == "HONDA") {
+    descuento = 0.05; //5%
+} else if (marca === "Yamaha" || marca == "yamaha" || marca == "YAMAHA") {
+    descuento = 0.08; //8%
+} else if (marca === "Susuki" || marca == "susuki" || marca == "SUSUKI") {
+    descuento = 0.1; //10%
+}
+
+let descuentoAplicado = precio * descuento
+let precioFinal = precio - descuentoAplicado
+
+console.log("Marca " + marca)
+console.log("Precio: $" + precio)
+console.log("Descuento: %" + descuento * 100)
+console.log("Precio Final: $" + precioFinal)
