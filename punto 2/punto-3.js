cd38438cdb6b1f1fca2763dbe2eaaf76679f7582
samuel florez <samuel.f.v.t.p@gmail.com>
2025-03-21 17:01:24 -0500

    se huizo el punto 2

--- punto 2/punto-3.js
+++ punto 2/punto-3.js
@@ -1,10 +1,10 @@
 
 // c significa calificacion
-let c1= 5;
-let c2= 4;
-let c3= 3;
-let c4= 3;
-let c5= 1;
+let c1= parseInt(prompt("ingrese la primera calificacion"));
+let c2= parseInt(prompt("ingrese la segunda calificacion"));
+let c3= parseInt(prompt("ingrese la tercera calificacion"));
+let c4= parseInt(prompt("ingrese la cuarta calificacion"));
+let c5= parseInt(prompt("ingrese la quinta calificacion"));
 
 let promedio = (c1+c2+c3+c4+c5)/5;
 
